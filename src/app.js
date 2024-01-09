beb96c77f26b3cbccbceda0d15660070853d0d1a
Rishueee <134083360+Rishueee@users.noreply.github.com>
2024-01-09 09:10:20 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const PLAYERS = [
-  'Spiderman',
-  'Captain America',
-  'Wonderwoman',
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
   // "Popcorn",
   // "Gemwoman",
   // "Bolt",
@@ -21,96 +21,67 @@ const PLAYERS = [
   // "Slingo"
 ];
 
-// Player Class
 class Player {
   constructor(id, name, type) {
-    // Progession 1: Create member variables and assign values
     this.id = id;
     this.name = name;
-    this.image = 'images/super-' + (id + 1) + '.png';
     this.strength = this.getRandomStrength();
+    this.image = "images/super-" + (id + 1) + ".png";
     this.type = type;
-    this.selected = false;
-    this.Victory = 0;
+    this.isthere = false;
   }
 
-  // Get random strength
   getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
   };
 
-  // Progression 2: Create a player for displaying
   view = () => {
-    let user = document.createElement('div');
-
-    user.classList.add('player');
-    
-    user.setAttribute('data-id', this.id);
-    
-    if (this.selected == true) user.classList.add('selected');
-    
-    let image = document.createElement('img');
-    
-    image.setAttribute('src', this.image);
-    
-    let name = document.createElement('div');
-    
+    let player = document.createElement("div");
+    player.classList.add("player");
+    player.setAttribute("data-id", this.id);
+    if (this.isthere == true) player.classList.add("selected");
+    let name = document.createElement("div");
     name.textContent = this.name;
-    
-    let strength = document.createElement('div');
-    
+    let strength = document.createElement("div");
     strength.textContent = this.strength;
-    
-    strength.className = 'strength';
-    
-    user.append(image, name, strength);
-    
-    return user;
+    strength.className = "strength";
+    let image = document.createElement("img");
+    image.setAttribute("src", this.image);
+    player.append(image, name, strength);
+
+    return player;
   };
 }
-
-// Superwar Class
 class Superwar {
-  
   constructor(players) {
-    // Progression 3:
-    // Create a field players
-    // Use Map method to loop through players argument and create new players
-    // Type your code here
-    this.players = players.map((user, index) => {
-  
-      let winning = index % 2 == 0 ? 'hero' : 'villain';
-  
-      return new Player(index, user, winning);
+    this.players = players.map((player, index) => {
+      const type = index % 2 === 0 ? "hero" : "villain";
+      return new Player(index, player, type);
     });
   }
 
-  // Display players in HTML
   viewPlayers = () => {
-   
-    fragment = this.buildPlayers('villain');
-    team.append(fragment);
+    let group = document.getElementById("heroes");
+    group.innerHTML = [];
+    let tags = this.buildPlayers("hero");
+    group.appendChild(tags);
 
-    team = document.getElementById('villains');
-    team.innerHTML = '';
-    let fragment = this.buildPlayers('hero');
-    team.append(fragment);
-    let team = document.getElementById('heroes');
-    team.innerHTML = '';
-    
+    group = document.getElementById("villains");
+    group.innerHTML = [];
+    tags = this.buildPlayers("villain");
+    group.appendChild(tags);
   };
 
-  // Build players fragment
   buildPlayers = (type) => {
-    let fragment = document.createDocumentFragment();
+    let tags = document.createDocumentFragment();
     this.players
-      .filter((player) => player.type == type)
-      .forEach((player) => fragment.append(player.view()));
-    return fragment;
+      .filter((player) => player.type === type)
+      .forEach((player) => tags.appendChild(player.view()));
+    return tags;
   };
 }
 
 window.onload = () => {
-  const hero = new Superwar(PLAYERS);
-  hero.viewPlayers();
+  const characters = new Superwar(PLAYERS);
+  characters.viewPlayers();
 };
